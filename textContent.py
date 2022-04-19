96267e73bfc6090038cf8efad0f3511a0c084bc5
kfr7 <kianranjbar7@gmail.com>
2022-04-19 11:28:17 -0700

    Made binarySearch function
    Stopword look up is faster now.

--- textContent.py
+++ textContent.py
@@ -2,6 +2,7 @@
 import lxml
 from bs4 import BeautifulSoup
 from collections import defaultdict
+import math
 import nltk
 nltk.download("punkt")
 # pip install nltk
@@ -10,6 +11,7 @@ nltk.download("punkt")
 # this one doesn't split contraction or hyphenated words
 
 stopWords = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some', 'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were', "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's", 'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves']
+biggestIndexForStopWords = len(stopWords) - 1
 
 def countTokens(resp):
     ''' Take in a response object and tokenize important
@@ -23,7 +25,23 @@ def countTokens(resp):
     for s in entireBodyTag.strings:
         tokenizedList = nltk.tokenize.word_tokenize(s.strip())
         for element in tokenizedList:
-            if element not in stopWords: # implement binary search for the sorted list of stopWords so its not O(n) but O(log n)
+            if not (binarySearch(element, 0, biggestIndexForStopWords)):
                 wordFreq[element] += 1
 
     return wordFreq
+
+def binarySearch(word, small_index, big_index):
+    '''Binary Search using the global stop words list to see
+    if the word given is a stop word or not. If it is a stop word, return
+    True (so don't put in dictionary). If false, put word in dictionary.'''
+    if small_index > big_index:
+        return False
+    else:
+        mid_index = math.floor((big_index+small_index)/2)
+        if word == stopWords[mid_index]:
+            return True
+        elif word < stopWords[mid_index]:
+            return binarySearch(word, small_index, mid_index-1)
+        else:
+            return binarySearch(word, mid_index+1, big_index)
+
